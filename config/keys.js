fcb845caa040f0a81725c7215779b5e33a1f9eb9
williamestrada <willy.estrada81@gmail.com>
2019-09-19 23:32:48 -0400

    Price functionality added, landing page styling

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb://william_admin:Kenlm8281!@yelpapp-shard-00-00-qy2nc.mongodb.net:27017,yelpapp-shard-00-01-qy2nc.mongodb.net:27017,yelpapp-shard-00-02-qy2nc.mongodb.net:27017/test?ssl=true&replicaSet=YelpApp-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+YelpCamp
+
+Yelp_Camp is Node, Express app using MongoDB, Passport.js Auth and Bootstrap for Styling. This Project was part of The Complete Web Developer BootCamp by Colt Steel.
+
+See Live at:
+
+https://desolate-temple-19053.herokuapp.com/
